ba2173217bb0a5ad6a4c6a27904504cd8710c317
Stefano Lattarini <stefano.lattarini@gmail.com>
2011-01-18 18:27:07 +0100

    new test on integer RSA, from http://people.eku.edu/styere/Encrypt/RSAdemo.html

--- tests/integer_encryption_test.py
+++ tests/integer_encryption_test.py
@@ -24,4 +24,20 @@ def test_wikipedia_sample():
 #  http://www.hanewin.net/encrypt/rsa/rsa-test.htm
 #  http://people.eku.edu/styere/Encrypt/RSAdemo.html
 
+def test_people_eku_edu():
+    p = 459983137786273
+    q = 167458717901777
+    e = 17
+    key = RSA.PrivateKey(p, q, e)
+    assert key.n == 77028186510125710835232907121
+    assert key.d == 45310697947132401996104246513
+    encrypter_priv = RSA.IntegerEncrypter(key)
+    encrypter_pub = RSA.IntegerEncrypter(key.public())
+    decrypter = RSA.IntegerDecrypter(key)
+    plain = 57128184570925880835232907122
+    cypher = 6764407817379484644525719120
+    assert (encrypter_priv.encrypt(plain) == cypher
+            and encrypter_pub.encrypt(plain) == cypher
+            and decrypter.decrypt(cypher) == plain)
+
 # vim: et sw=4 ts=4 ft=python
