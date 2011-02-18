6aeca2b3b812e55c32476aae845cdb7b02272b2b
Stefano Lattarini <stefano.lattarini@gmail.com>
2011-02-18 19:37:37 +0100

    tests on bytes <-> integers conversion: refactor & extend

--- tests/bytes_integer_conversions_test.py
+++ tests/bytes_integer_conversions_test.py
@@ -6,54 +6,141 @@
 conversions."""
 
 import pytest
+import random
 from tests.lib import with_params, without_duplicates, pytest_generate_tests
 from tests.lib import infinite_iteration, seq2gen, TestError
 from RSA import ByteSequenceEncrypter, CryptoException
 
-miscellaneous_integers_with_17_or_more_bits = [
-    1<<16, 1<<17, 1<<23, 1<<24, 1<<25, 1<<31, 1<<32, 1<<64, 1<<65,
-    2**16 + 1, 2**16 + 55, 2**16 + 2**13, 2**17 - 1, 2**17 + 523,
-    2**31 + 45673, 2**32 + 72, 3 * 2**32 + 679, 2**50, 3**100,
-    5**123 + 7**43, 13**57 + 12, 2**1000,  2**2000 + 3**1000
-]
+# Return "some" random positive integers that requires, to be represented,
+# a number of bits `n_bits' with l <= nbits <= u.
+def some_ints_with_bits(l, u, count=20):
+    assert (1 <= l <= u and count > 0)
+    L = 1 << (l - 1)
+    U = (1 << u) - 1
+    yield L
+    yield L + 1
+    for i in range(0, count):
+        yield random.randint(L, U)
+    yield U - 1
+    yield U
 
-#miscellaneous_integers_with_9_or_more_bits = [
-#    1<<8, 1<<9, 1<<15, 1<<16,
-#    2**8 + 1, 2**8 + 55, 2**8 + 2**5, 2**9 - 1, 2**9 + 523
-#] + miscellaneous_integers_with_17_or_more_bits
+# Return a random list of integers n that should be represented by the
+# given number of bytes `n_byte'.  This means that n should require, to
+# be represented, a number of bits 'x' with:
+#   8 * n_byte - 7 <= x <= 8 * n_byte
+def n_with_bytes(n_byte):
+    return some_ints_with_bits(8 * n_byte - 7, 8 * n_byte)
 
-# Return a list of integers that should cause the plaintexts to be
-# broken into chunks of size (in bytes) = n_byte.  This means that
-# n should require, to be represented, a number of bits 'x' with:
-#   8 * n_byte + 8 <= x <= 8 * n_byte + 15.
+# Return a random list of integers that, when used as the 'n' entry of a
+# key, should cause the plaintexts to be broken into chunks of size (in
+# bytes) = n_byte.
 def n_with_plaintext_chunklen(n_byte):
-    list_of_n = []
-    n_bit = 8 * n_byte + 8
-    for i in range(n_bit, n_bit + 8):
-        n = 1 << i
-        list_of_n.extend([n, n + 1, n + 16, n + 55])
-        if i < n_bit + 7:
-            list_of_n.extend([n * 3, n * 3 + 1, n * 3 + 74])
-    return list_of_n
+    return some_ints_with_bits(8 * n_byte + 9, 8 * n_byte + 16)
 
-# XXX: still hacky/rough
+length_data = eval("""[
+#------------5432109876543210987654321----------------
+    dict(n=0b........................., pc_len=None),
+    dict(n=0b........................1, pc_len=None),
+    dict(n=0b.......................1., pc_len=None),
+    dict(n=0b......................1.., pc_len=None),
+    dict(n=0b......................111, pc_len=None),
+    dict(n=0b.....................1..., pc_len=None),
+    dict(n=0b..................1......, pc_len=None),
+    dict(n=0b.................1......., pc_len=None),
+    dict(n=0b.................11111111, pc_len=None),
+#------------5432109876543210987654321----------------
+    dict(n=0b................1........, pc_len=None),
+    dict(n=0b................1.1.11.11, pc_len=None),
+    dict(n=0b...............1........., pc_len=None),
+    dict(n=0b..............1.........., pc_len=None),
+    dict(n=0b.............1..........., pc_len=None),
+    dict(n=0b...........1............., pc_len=None),
+    dict(n=0b...........1..11..11..1.1, pc_len=None),
+    dict(n=0b..........1.............., pc_len=None),
+    dict(n=0b.........1..............., pc_len=None),
+    dict(n=0b.........1111111111111111, pc_len=None),
+#------------5432109876543210987654321----------------
+    dict(n=0b........1................, pc_len=1),
+    dict(n=0b........11111111111111111, pc_len=1),
+    dict(n=0b.......1................., pc_len=1),
+    dict(n=0b.......1.1.1.1.1.1.1.1.1., pc_len=1),
+    dict(n=0b......1.................., pc_len=1),
+    dict(n=0b.....1..................., pc_len=1),
+    dict(n=0b....1...................., pc_len=1),
+    dict(n=0b...1....................., pc_len=1),
+    dict(n=0b..1......................, pc_len=1),
+    dict(n=0b.1......................., pc_len=1),
+    dict(n=0b.111111111111111111111111, pc_len=1),
+#------------5432109876543210987654321---------------
+    dict(n=0b1........................, pc_len=2),
+#----------------------------------------------------
+    dict(n=1<<26,      pc_len=2),
+    dict(n=1<<27,      pc_len=2),
+    dict(n=1<<28,      pc_len=2),
+    dict(n=1<<29,      pc_len=2),
+    dict(n=1<<30,      pc_len=2),
+    dict(n=1<<31,      pc_len=2),
+    dict(n=1<<32 - 1,  pc_len=2),
+    dict(n=1<<32,      pc_len=3),
+    dict(n=1<<33,      pc_len=3),
+    dict(n=1<<47,      pc_len=4),
+    dict(n=1<<48 - 1,  pc_len=4),
+    dict(n=1<<48,      pc_len=5),
+    dict(n=1<<49,      pc_len=5),
+    dict(n=1<<800,     pc_len=99),
+    dict(n=1<<807,     pc_len=99),
+    dict(n=1<<808 - 1, pc_len=99),
+    dict(n=1<<808,     pc_len=100),
+    dict(n=1<<809,     pc_len=100),
+#-------------------------------------------------------
+]""".replace('.', '0'))
+
+# XXX: some largish known I/O pairs would be nice.
 def generate_cipher_conversion_data():
     data = []
-    for n_byte in (2, 3, 4, 11, 64, 101, 1023, 1024, 1025):
-        n = 1 << (n_byte * 8)
-        data.append(dict(n=n, bytes='', ints=[]))
-        data.append(dict(n=n, bytes='\000' * (n_byte+1), ints=[0x00]))
-        for x in [
-            '00', '01', '02', '03', '07', '09', '0a', '41', '61',
-            '6a', '7e', '80', '81', '94', 'f1', 'fe', 'ff'
-        ]:
-            ints = [eval("0x%s" % x)]
-            bytes = eval("'\\x%s'" % x) + '\000' * n_byte
-            data.append(dict(n=n, bytes=bytes, ints=ints))
+    for n_byte in (3, 4, 5, 12, 32, 1000):
+        for n in n_with_bytes(n_byte):
+            data.append(dict(n=n, bytes='', ints=[]))
+            data.append(dict(n=n, bytes='\x00' * n_byte, ints=[0x00]))
+            for x in [
+                '00', '01', '02', '03', '07', '09', '0a', '41', '61',
+                '6a', '7e', '80', '81', '94', 'f1', 'fe', 'ff'
+            ]:
+                bytes = eval("'\\x%s'" % x)
+                bytes += '\x00' * (n_byte - 1)
+                ints = [eval("0x%s" % x)]
+                data.append(dict(n=n, bytes=bytes, ints=ints))
+            for x in [
+                '00:00', '00:01', '01:00', '02:03', '07:0a', '43:67',
+                '5a:f3', '7e:0f', '80:81', '94:b1', 'bc:ff', '1b:ea',
+            ]:
+                x1, x2 = x.split(':')
+                bytes = eval("'\\x%s\\x%s'" % (x1, x2))
+                bytes +='\x00' * (n_byte - 2)
+                ints = [eval("0x%s%s" % (x2, x1))]
+                data.append(dict(n=n, bytes=bytes, ints=ints))
+    raw_data = [
+        dict(
+            n_byte = 3,
+            bytes = '\x44\x3a\x00\x11\xfe\x00',
+            ints = [0x3a44, 0xfe11],
+        ),
+    ]
+    for d in raw_data:
+        n_byte = d['n_byte']
+        del d['n_byte']
+        for n in n_with_bytes(n_byte):
+            data.append(dict(d, n=n))
     return data
 
 def generate_plain_conversion_data():
     data = []
+    miscellaneous_integers_with_17_or_more_bits = [
+        1<<16, 1<<17, 1<<23, 1<<24, 1<<25, 1<<31, 1<<32, 1<<64, 1<<65,
+        2**16 + 1, 2**16 + 55, 2**16 + 2**13, 2**17 - 1, 2**17 + 523,
+        2**31 + 45673, 2**32 + 72, 3 * 2**32 + 679, 2**50, 3**100,
+        5**123 + 7**43, 13**57 + 12, 2**1000,  2**2000 + 3**1000
+    ]
     for n in miscellaneous_integers_with_17_or_more_bits:
         data.append(dict(n=n, bytes='', ints=[]))
         data.append(dict(n=n, bytes='\000', ints=[0xff00]))
@@ -177,66 +264,20 @@ def generate_plain_conversion_data():
             data.append(dict(d, n=n))
     return data
 
+@without_duplicates
+def generate_too_small_n():
+    list_of_small_n = []
+    for d in length_data:
+        if d['pc_len'] is None:
+            list_of_small_n.append(d['n'])
+    for i in 1, 2:
+        list_of_small_n.extend(n_with_bytes(i))
+    return list_of_small_n
+
 plain_conversion_data = generate_plain_conversion_data()
 cipher_conversion_data = generate_cipher_conversion_data()
-
-length_data = eval("""[
-#------------5432109876543210987654321----------------
-    dict(n=0b........................., pc_len=None),
-    dict(n=0b........................1, pc_len=None),
-    dict(n=0b.......................1., pc_len=None),
-    dict(n=0b......................1.., pc_len=None),
-    dict(n=0b......................111, pc_len=None),
-    dict(n=0b.....................1..., pc_len=None),
-    dict(n=0b..................1......, pc_len=None),
-    dict(n=0b.................1......., pc_len=None),
-    dict(n=0b.................11111111, pc_len=None),
-#------------5432109876543210987654321----------------
-    dict(n=0b................1........, pc_len=None),
-    dict(n=0b................1.1.11.11, pc_len=None),
-    dict(n=0b...............1........., pc_len=None),
-    dict(n=0b..............1.........., pc_len=None),
-    dict(n=0b.............1..........., pc_len=None),
-    dict(n=0b...........1............., pc_len=None),
-    dict(n=0b...........1..11..11..1.1, pc_len=None),
-    dict(n=0b..........1.............., pc_len=None),
-    dict(n=0b.........1..............., pc_len=None),
-    dict(n=0b.........1111111111111111, pc_len=None),
-#------------5432109876543210987654321----------------
-    dict(n=0b........1................, pc_len=1),
-    dict(n=0b........11111111111111111, pc_len=1),
-    dict(n=0b.......1................., pc_len=1),
-    dict(n=0b.......1.1.1.1.1.1.1.1.1., pc_len=1),
-    dict(n=0b......1.................., pc_len=1),
-    dict(n=0b.....1..................., pc_len=1),
-    dict(n=0b....1...................., pc_len=1),
-    dict(n=0b...1....................., pc_len=1),
-    dict(n=0b..1......................, pc_len=1),
-    dict(n=0b.1......................., pc_len=1),
-    dict(n=0b.111111111111111111111111, pc_len=1),
-#------------5432109876543210987654321---------------
-    dict(n=0b1........................, pc_len=2),
-#----------------------------------------------------
-    dict(n=1<<26,      pc_len=2),
-    dict(n=1<<27,      pc_len=2),
-    dict(n=1<<28,      pc_len=2),
-    dict(n=1<<29,      pc_len=2),
-    dict(n=1<<30,      pc_len=2),
-    dict(n=1<<31,      pc_len=2),
-    dict(n=1<<32 - 1,  pc_len=2),
-    dict(n=1<<32,      pc_len=3),
-    dict(n=1<<33,      pc_len=3),
-    dict(n=1<<47,      pc_len=4),
-    dict(n=1<<48 - 1,  pc_len=4),
-    dict(n=1<<48,      pc_len=5),
-    dict(n=1<<49,      pc_len=5),
-    dict(n=1<<800,     pc_len=99),
-    dict(n=1<<807,     pc_len=99),
-    dict(n=1<<808 - 1, pc_len=99),
-    dict(n=1<<808,     pc_len=100),
-    dict(n=1<<809,     pc_len=100),
-#-------------------------------------------------------
-]""".replace('.', '0'))
+too_small_n = generate_too_small_n()
+print too_small_n
 
 class ByteSeqConverter(ByteSequenceEncrypter):
     def __init__(self, x):
@@ -251,7 +292,7 @@ class ByteSeqConverter(ByteSequenceEncrypter):
 def test_plain_chunk_length(n, chunk_length):
     assert chunk_length == ByteSeqConverter(n).plain_chunk_byte_length
 
-@with_params([d['n'] for d in length_data if d['pc_len'] is None], 'n')
+@with_params(too_small_n, 'n')
 def test_plain_chunk_length_key_too_small(n):
     pytest.raises(CryptoException, "ByteSeqConverter(n)")
 
@@ -316,13 +357,6 @@ def test_p2i_or_i2p_with_infinite_generator(n_byte, n_iter, convtype):
         assert chunk_expected == chunk_got
         if iter_count >= n_iter:
             break
-
-@with_params([1 << i for i in range(0, 16)] +
-             [3, 55, 80, 157, 999, 1000, 63123, 2**16 - 1], 'n')
-@with_params(['', '\000', 'a', 'bc', 'x'*10, 'x'*773], 'bytes')
-def test_p2i_key_too_small(n, bytes):
-    pytest.raises(CryptoException,
-                  "for _ in ByteSeqConverter(n).p2i(bytes): pass")
 
 # Check that we can convert also "big" byte sequences (~ 200M) in a
 # reasonable time.
